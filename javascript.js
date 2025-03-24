91aa2ef54bc3a1269c358ba770578599177f5cd7
adai9993 <adai9993@student.acitech.org>
2025-03-24 09:12:56 -0400

    -used coding from a video to better understand what to do -this helped get the buttons to change color and have multiple options - I acomplished everything besides getting the buttons actually coding to work

--- javascript.js
+++ javascript.js
@@ -1,17 +1,19 @@
 // ROCK PAPER SCISSORS
 
-const choices = ["rock", "paper", "scissors"];
+const choices = ["rock", "paper", "scissors", "Mr_C"];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
 const resultDisplay = document.getElementById("resultDisplay");
 const playerScoreDisplay = document.getElementById("playerScoreDisplay");
 const computerScoreDisplay = document.getElementById("computerScoreDisplay");
-let playerScore = 0;     
-let computerScore = 0;    
+let playerScore = 0;
+let computerScore = 0;
+
+  
 
 function playGame(playerChoice) {
     
-    const computerChoice = choices[Math.floor(Math.random() * 3)];
+    const computerChoice = choices[Math.floor(Math.random() * 4)];
     let result = ""; 
 
     if(playerChoice === computerChoice){
@@ -28,10 +30,29 @@ function playGame(playerChoice) {
              case "scissors":
                 result = (computerChoice === "paper") ? "YOU WIN" : "YOU Lose:(" ;
                 break;
+                case "Mr_C":
+                    result = (computerChoice === "rock") ? "YOU WIN" : "YOU Lose:(" ;
+                    break;
          }
 } 
-playerDisplay.textContent = 'PLAYER: ${playerchoice}' ;
-computerDisplay.textContent = 'COMPUTER: ${computerchoice}' ;
-resultDisplay.textContent = result;
+    playerDisplay.textContent = `PLAYER: ${playerChoice}` ;
+    computerDisplay.textContent = `COMPUTER: ${computerChoice}` ;
+    resultDisplay.textContent = result;
+
+    resultDisplay.classList.remove("greenText","redText")
+
+
+    switch(result){
+        case "YOU WIN!":
+            resultDisplay.classList.add("greenText");
+            playerScore++;
+            playerScoreDisplay.textContent = playerScore;
+            break;
+        case "YOU LOSE!":
+            resultDisplay.classList.add("redText");
+            computerScore++;
+            computerScoreDisplay.textContent = computerScore;
+             break;
+    }
 
 }
